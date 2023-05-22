26bb13a92f5db619a062448fc2eb6744c2f13a6d
fr03e1 <aleksejbocarov65@gmail.com>
2023-05-22 17:27:39 +0300

    add jwt auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
+
+Route::group(['middleware'=> 'auth:api'],function () {
+    Route::get('/test',function () {
+        return 'hello';
+    });
+});
+
 
